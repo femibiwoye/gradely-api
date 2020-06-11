3b0d7443aadbfdb983237c83f14923308682d645
Akagha Chinaka <akaghachinaka@gmail.com>
2020-06-11 04:38:22 +0100

    fixing cors error
    fixing cors error

--- config/web.php
+++ config/web.php
@@ -1,12 +1,5 @@
 <?php
 
-
-header('Access-Control-Allow-Origin: *'); 
-header("Access-Control-Allow-Credentials: false");
-header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-header('Access-Control-Max-Age: 8000');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
-
 $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
 
--- web/index.php
+++ web/index.php
@@ -1,5 +1,11 @@
 <?php
 
+header('Access-Control-Allow-Origin: *'); 
+header("Access-Control-Allow-Credentials: false");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 8000');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
