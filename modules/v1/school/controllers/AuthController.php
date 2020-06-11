c935e5eb85e8871db69be8fd45ada935bf4ad0de
femibiwoye <femibiwoye@gmail.com>
2020-06-11 19:14:48 +0100

    Update 3 cors control to controller behavior

--- modules/v1/school/controllers/AuthController.php
+++ modules/v1/school/controllers/AuthController.php
@@ -22,8 +22,11 @@ class AuthController extends ActiveController
 
     public function beforeAction($action)
     {
-        $this->request = \yii::$app->request->post();
-        return parent::beforeAction($action);
+        if (parent::beforeAction($action)) {
+            \Yii::$app->response->format = Response::FORMAT_JSON;
+            return true;
+        }
+
     }
 
 
@@ -59,13 +62,41 @@ class AuthController extends ActiveController
 
     public function behaviors()
     {
+
         $behaviors = parent::behaviors();
 
+        // remove authentication filter necessary because we need to
+        // add CORS filter and it should be added after the CORS
+        unset($behaviors['authenticator']);
+
+        // add CORS filter
         $behaviors['corsFilter'] = [
-            'class'=>\yii\filters\Cors::className()
+            'class' => '\yii\filters\Cors',
+            'cors' => [
+                'Origin' => ['*'],
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                'Access-Control-Request-Headers' => ['*'],
+            ],
         ];
 
+        // re-add authentication filter of your choce
+//        $behaviors['authenticator'] = [
+//            'class' => yii\filters\auth\HttpBasicAuth::class
+//        ];
+
+        // avoid authentication on CORS-pre-flight requests (HTTP OPTIONS method)
+        $behaviors['authenticator']['except'] = ['options'];
         return $behaviors;
+
+
+
+//        $behaviors = parent::behaviors();
+//
+//        $behaviors['corsFilter'] = [
+//            'class'=>\yii\filters\Cors::className()
+//        ];
+//
+//        return $behaviors;
     }
 
     /**
