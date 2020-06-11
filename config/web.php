e4be1da954a6d61808d84878af85ef5276c298c7
Akagha Chinaka <akaghachinaka@gmail.com>
2020-06-11 04:15:56 +0100

    fixing cors error
    fixing cors error

--- config/web.php
+++ config/web.php
@@ -2,7 +2,7 @@
 
 
 header('Access-Control-Allow-Origin: *'); 
-header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Credentials: false");
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 header('Access-Control-Max-Age: 8000');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
