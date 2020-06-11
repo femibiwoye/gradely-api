8c9b35f0d12f40ee476e4d4dd2f2bcce77350622
femibiwoye <femibiwoye@gmail.com>
2020-06-11 11:46:01 +0100

    Confirmation that auto deployment is working+4

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -257,7 +257,7 @@ class AuthController extends ActiveController
         return[
             'code' => 200,
             'message' => 'Ok',
-            'Addition'=>'Confirmation that auto deployment is working 3',
+            'Addition'=>'Confirmation that auto deployment is working 4',
             'data' => ['user' => $model->getUser()],
             'expiry' => $tokenExpires,
             'token' => $authKey
