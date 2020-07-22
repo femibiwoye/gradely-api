b834a587dd0e879127dd70d291997b2e42ab6088
Femi Ibiwoye <femibiwoye@gmail.com>
2020-07-22 17:50:33 +0100

    Revert "- improvements suggested in retrieving homeworks in the class"
    This reverts commit 751d532c4d2577f99ca6d70cbdc075d8e3805ba1.
    
    # Conflicts:
    #	config/urls.php
    #	modules/v2/models/Classes.php
    #	modules/v2/teacher/controllers/HomeworkController.php

---
+++ config/urls.php
@@ -0,0 +1,56 @@
+<?php
+
+return [
+    //Authentications
+    'POST v2/login' => 'v2/auth/login',
+    'POST v2/signup/<type:[a-z]+>' => 'v2/auth/signup',
+    'POST v2/logout' => 'v2/auth/logout',
+    'POST v2/forgot-password' => 'v2/auth/forgot-password',
+    'POST v2/reset-password' => 'v2/auth/reset-password',
+
+    //Onboarding
+    'PUT v2/general/update-boarding' => 'v2/general/update-boarding',
+    'GET v2/general/boarding-status' => 'v2/general/boarding-status',
+
+    //Current User
+    'GET v2/general/user' => 'v2/general/user',
+
+    //Services
+    'GET v2/services/cloudinary' => 'v2/services/cloudinary',
+
+    //Teacher Profile
+    'PUT v2/teacher/profile/update-email' => 'v2/teacher/profile/update-email',
+    'PUT v2/teacher/profile/update-password' => 'v2/teacher/profile/update-password',
+    'PUT v2/teacher/profile' => 'v2/teacher/profile/update',
+    'GET v2/teacher/profile/preference' => 'v2/teacher/profile/preference',
+    'PUT v2/teacher/profile/preference' => 'v2/teacher/profile/update-preference',
+    'DELETE v2/teacher/profile/delete-account' => 'v2/teacher/profile/delete-account',
+
+    //Teacher classes
+    'DELETE v2/teacher/student/remove/<student_id:\d+>/<class_id:\d+>' => 'v2/teacher/class/delete-student',
+    'GET v2/teacher/student/<id:\d+>' => 'v2/teacher/class/get-student',
+    'GET v2/teacher/students/<class_id:\d+>' => 'v2/teacher/class/students-in-class',
+    'GET v2/teacher/search-school' => 'v2/teacher/class/search-school',
+    'GET v2/teacher/class' => 'v2/teacher/class/teacher-class',
+    'GET v2/teacher/class/school/<id:\d+>' => 'v2/teacher/class/school',
+    'GET v2/teacher/class/<code:[a-zA-Z0-9/]+>' => 'v2/teacher/class/view',
+    'POST v2/teacher/class/add-teacher' => 'v2/teacher/class/add-teacher',
+    'POST v2/teacher/class/add-teacher-class' => 'v2/teacher/class/add-teacher-school',
+    'POST v2/teacher/student/add-multiple' => 'v2/teacher/class/add-student',
+
+    //Feed Class
+    'GET v2/teacher/upcoming' => 'v2/teacher/feed/upcoming',
+    'POST v2/teacher/comment/<post_id:\d+>' => 'v2/teacher/feed/feed-comment',
+    'POST v2/teacher/like/<post_id:\d+>' => 'v2/teacher/feed/feed-like',
+    'POST v2/teacher/like-comment/<comment_id:\d+>' => 'v2/teacher/feed/comment-like',
+    'POST v2/teacher/announcement' => 'v2/teacher/feed/create',
+    'GET v2/teacher/feed' => 'v2/teacher/feed/index',
+
+    //Homework Class
+    'GET v2/teacher/homework/<class_id:\d+>' => 'v2/teacher/homework/class-homeworks',
+    'GET v2/teacher/homework/<homework_id:\d+>' => 'v2/teacher/homework/homework',
+    'DELETE v2/teacher/homework/<homework_id:\d+>' => 'v2/teacher/homework/delete-homework',
+    'PUT v2/teacher/homework/extend/<homework_id:\d+>' => 'v2/teacher/homework/extend-date',
+
+
+];
---
+++ modules/v2/models/Classes.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace app\modules\v2\models;
+
+use Yii;
+use yii\behaviors\SluggableBehavior;
+
+/**
+ * This is the model class for table "classes".
+ *
+ * @property int $id
+ * @property int $school_id
+ * @property int $global_class_id
+ * @property string $slug
+ * @property string $class_name e.g Senior secondary School 1
+ * @property string $abbreviation
+ * @property string $class_code e.g HBY/SSS1
+ * @property string $created_at
+ */
+class Classes extends \yii\db\ActiveRecord
+{
+	const SCENERIO_CREATE_CLASS = 'create_class';
+	const SCENERIO_UPDATE_CLASS = 'update_class';
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function tableName()
+	{
+		return 'classes';
+	}
+
+    /**
+     * Get and ensure unique name for class slug and class abbreviation
+     * @return array
+     */
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => SluggableBehavior::className(),
+                'attribute' => 'class_name',
+                'ensureUnique' => true
+            ],
+            [
+                'class' => SluggableBehavior::className(),
+                'attribute' => 'abbreviation',
+                //'ensureUnique' => true,
+                'slugAttribute' => 'abbreviation'
+            ]
+
+        ];
+    }
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function rules()
+	{
+		return [
+			//[['school_id', 'global_class_id', 'slug', 'class_name', 'abbreviation', 'class_code'], 'required'],
+			[['school_id', 'global_class_id'], 'integer'],
+			[['created_at'], 'safe'],
+			[['slug', 'class_name'], 'string', 'max' => 255],
+			[['abbreviation', 'class_code'], 'string', 'max' => 20],
+			[['class_code'], 'unique'],
+			[['global_class_id','class_name','class_code'], 'required',
+				 'on' => self::SCENERIO_CREATE_CLASS
+			],
+			[['global_class_id','class_name','abbreviation'], 'required',
+				 'on' => self::SCENERIO_UPDATE_CLASS
+			]
+		];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function attributeLabels()
+	{
+		return [
+			'id' => 'ID',
+			'school_id' => 'School ID',
+			'global_class_id' => 'Global Class ID',
+			'slug' => 'Slug',
+			'class_name' => 'Class Name',
+			'abbreviation' => 'Abbreviation',
+			'class_code' => 'Class Code',
+			'created_at' => 'Created At',
+		];
+	}
+
+	/*public function fields() {
+		return [
+			'id',
+			'school_id',
+			'global_class_id',
+			'slug',
+			'class_name',
+			'abbreviation',
+			'class_code',
+			'created_at'
+		];
+	}*/
+
+	public function getSchool() {
+		return $this->hasOne(Schools::className(), ['id' => 'school_id']);
+	}
+
+    public function getGlobalClass() {
+        return $this->hasOne(GlobalClass::className(), ['id' => 'global_class_id']);
+    }
+
+
+    public function getStudentSchool()
+    {
+        return $this->hasMany(StudentSchool::className(), ['class_id' => 'id']);
+    }
+
+    public function getHomeworks() {
+    	return $this->hasMany(Homeworks::className(), ['class_id' => 'id']);
+    }
+}
