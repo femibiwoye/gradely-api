b825898706ce9c22207a1d1d7b0f49459aef7e3f
tolu-six <tolu58@gmail.com>
2020-08-26 15:08:11 +0100

    Homework Question

--- modules/v2/student/controllers/PracticeController.php
+++ modules/v2/student/controllers/PracticeController.php
@@ -3,8 +3,16 @@
 namespace app\modules\v2\student\controllers;
 
 use app\modules\v1\models\StudentSchool;
+use app\modules\v2\components\SharedConstant;
 use app\modules\v2\models\ApiResponse;
+use app\modules\v2\models\Classes;
+use app\modules\v2\models\HomeworkQuestions;
 use app\modules\v2\models\Homeworks;
+use app\modules\v2\models\Questions;
+use app\modules\v2\models\QuizSummary;
+use app\modules\v2\models\Subjects;
+use app\modules\v2\models\SubjectTopics;
+use app\modules\v2\models\TeacherClass;
 use yii\filters\auth\HttpBearerAuth;
 use yii\rest\Controller;
 
@@ -65,6 +73,60 @@ class PracticeController extends Controller
         }
 
         return (new ApiResponse)->success($homework, ApiResponse::SUCCESSFUL, 'Homework retrieved');
+
+    }
+
+    public function actionStartHomework(){
+
+        $homework_id = \Yii::$app->request->post('homework_id');
+
+        $homework = Homeworks::find()
+                    ->innerJoin('quiz_summary', 'quiz_summary.homework_id = homeworks.id')
+                    ->andWhere(['homeworks.id' => $homework_id, 'homeworks.student_id' => \Yii::$app->user->id]);
+
+        if($homework->andWhere(['quiz_summary.submit' => SharedConstant::VALUE_ZERO])->exists()){
+            return (new ApiResponse)->error(null, ApiResponse::UNABLE_TO_PERFORM_ACTION, 'Student has not submitted homework');
+        }
+
+        if($homework->andWhere(['quiz_summary.submit' => SharedConstant::VALUE_ONE])->exists()){
+            return (new ApiResponse)->error(null, ApiResponse::UNABLE_TO_PERFORM_ACTION, 'Student has submitted homework');
+        }
+
+        $model = new \yii\base\DynamicModel(compact('class_id', 'type', 'subject_id', 'topic_id', 'question_count', 'duration', 'teacher_id', 'homework_id'));
+        $model->addRule(['homework_id'], 'exist', ['targetClass' => Homeworks::className(), 'targetAttribute' => ['homework_id' => 'id', 'student_id' => 'student_id']]);
+        $model->addRule(['class_id'], 'exist', ['targetClass' => \app\modules\v2\models\StudentSchool::className(), 'targetAttribute' => ['class_id' => 'class_id', 'student_id' => 'student_id']]);
+        $model->addRule(['subject_id'], 'exist', ['targetClass' => Subjects::className(), 'targetAttribute' => ['subject_id' => 'id']]);
+        //$model->addRule(['topic_id'], 'exist', ['targetClass' => SubjectTopics::className(), 'targetAttribute' => ['topic_id' => 'id', 'subject_id' => 'subject_id']]);
+        $model->addRule(['type'], 'in', ['range' => ['homework']]);
+        if (!$model->validate()) {
+            return (new ApiResponse)->error($model->getErrors(), ApiResponse::UNABLE_TO_PERFORM_ACTION, 'Question not validated');
+        }
+
+        $model = new QuizSummary();
+        $model->attributes = \Yii::$app->request->post();
+        $model->teacher_id = $homework->teacher_id;
+        $model->student_id = $homework->student_id;
+        $model->class_id = $homework->student_id;
+        $model->subject_id = $homework->subject_id;
+        $model->type = 'homework';
+
+        if (!$model->validate()) {
+            return (new ApiResponse)->error($model->getErrors(), ApiResponse::UNABLE_TO_PERFORM_ACTION, 'Question not validated');
+        }
+
+        $homework_questions = HomeworkQuestions::find()->alias('hq')
+                              ->innerJoin('homeworks', 'homeworks.id = hq.homework_id')
+                              ->innerJoin('questions', 'questions.id = hq.question_id')
+                              ->andWhere(['hq.homework_id' => $homework_id])
+                              ->all();
+        $model->question_count = Count($homework_questions);
+
+        if(!$model->save()){
+            return (new ApiResponse)->error(null, ApiResponse::UNABLE_TO_PERFORM_ACTION, 'No homework assigned for you');
+        }
+
+        return (new ApiResponse)->success($homework_questions, ApiResponse::SUCCESSFUL, 'Homework questions retrieved');
+
 
     }
 }
