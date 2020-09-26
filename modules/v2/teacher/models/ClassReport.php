a60552e7bc74600309a0dd94f563ca0086a23bf0
Femi Ibiwoye <femibiwoye@gmail.com>
2020-09-26 11:38:30 +0100

    Update ClassReport.php

--- modules/v2/teacher/models/ClassReport.php
+++ modules/v2/teacher/models/ClassReport.php
@@ -141,9 +141,9 @@ class ClassReport extends Model
         $struggling = [];
 
         foreach ($students as $student) {
-            array_push($student, $this->getRecommendations($student['id']));
-            $student['recommendations'] = $student[SharedConstant::VALUE_ZERO];
-            unset($student[SharedConstant::VALUE_ZERO]);
+            //array_push($student, $this->getRecommendations($student['id']));
+            //$student['recommendations'] = $student[SharedConstant::VALUE_ZERO];
+            //unset($student[SharedConstant::VALUE_ZERO]);
             if ($student['score'] >= 75) {
                 $excellence[] = $student;
             } elseif ($student['score'] >= 50 && $student['score'] < 75) {
