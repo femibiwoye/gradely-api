1ceb0fb963db93803b82663b235deba9eae4eb22
Femi Ibiwoye <femibiwoye@gmail.com>
2020-10-04 23:38:26 +0100

    Finalize start class, join class, end class and update video.

--- config/learning-url.php
+++ config/learning-url.php
@@ -3,8 +3,10 @@
 return [
 
     //Live class and video conferencing
-    'GET v2/update-live-class-video' => 'v2/learning/live-class/update-live-class-video', //
+    'GET v2/update-live-class-video/<filename:[a-zA-Z0-9_/]+>' => 'v2/learning/live-class/update-live-class-video', //
     'PUT v2/learning/live-class/start-class' => 'v2/learning/live-class/start-class', //
+    'PUT v2/learning/live-class/join-class' => 'v2/learning/live-class/join-class', //
+    'PUT v2/learning/live-class/end-class' => 'v2/learning/live-class/end-class', //
 //
 //    ['class' => 'yii\rest\UrlRule', 'controller' => ['v2/learning/live-class'], 'extraPatterns' => [
 //        'PUT start-class' => 'start-class',
