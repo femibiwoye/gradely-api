adccf3876bd7278e5362355969b1ce54719b337b
Sami Ullah <samiullah@smsami.com>
2020-10-05 16:29:52 +0500

    - update title and subject_id

--- modules/v2/controllers/FeedController.php
+++ modules/v2/controllers/FeedController.php
@@ -304,6 +304,33 @@ class FeedController extends ActiveController
         return (new ApiResponse)->success(null, ApiResponse::SUCCESSFUL, 'Record deleted successfully!');
     }
 
+    public function actionUpdate($id)
+    {
+        $subject_id = Yii::$app->request->post('subject_id');
+        $title = Yii::$app->request->post('title');
+        $validate = new \yii\base\DynamicModel(compact('subject_id', 'title'));
+        $validate->addRule(['subject_id', 'title'], 'required');
+        if (!$validate->validate()) {
+            return (new ApiResponse)->error($validate->getErrors(), ApiResponse::UNABLE_TO_PERFORM_ACTION);
+        }
+
+        $model = TutorSession::find()
+                    ->where(['id' => $id, 'requester_id' => Yii::$app->user->id])
+                    ->one();
+
+        if (!$model) {
+            return (new ApiResponse)->error(null, ApiResponse::UNABLE_TO_PERFORM_ACTION, 'Record not found');
+        }
+
+        $model->subject_id = $subject_id;
+        $model->title = $title;
+        if (!$model->save()) {
+            return (new ApiResponse)->error($model->getErrors(), ApiResponse::UNABLE_TO_PERFORM_ACTION, 'Record not updated');
+        }
+
+        return (new ApiResponse)->success($model, ApiResponse::SUCCESSFUL, 'Record updated successfully');
+    }
+
     /**
      *
      * Delete records from feed
