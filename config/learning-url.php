173140a4af2af84a1632de22c324bccf159f1482
Femi Ibiwoye <femibiwoye@gmail.com>
2020-10-05 06:06:41 +0100

    Fixes on accepted name after session.

--- config/learning-url.php
+++ config/learning-url.php
@@ -3,7 +3,7 @@
 return [
 
     //Live class and video conferencing
-    'GET v2/update-live-class-video/<filename:[a-zA-Z0-9_/]+>' => 'v2/learning/live-class/update-live-class-video', //
+    'GET v2/update-live-class-video/<filename:[a-zA-Z0-9._/]+>' => 'v2/learning/live-class/update-live-class-video', //
     'PUT v2/learning/live-class/start-class' => 'v2/learning/live-class/start-class', //
     'PUT v2/learning/live-class/join-class' => 'v2/learning/live-class/join-class', //
     'PUT v2/learning/live-class/end-class' => 'v2/learning/live-class/end-class', //
