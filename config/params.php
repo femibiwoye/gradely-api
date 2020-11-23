64968d087d8f4c6c6efd2f6ac6923f8e480c3b88
Femi Ibiwoye <femibiwoye@gmail.com>
2020-11-23 15:45:43 +0100

    Update

--- config/params.php
+++ config/params.php
@@ -22,7 +22,7 @@ return [
     'payment_sk' => PAYSTACK_SK,
     'payment_pk' => PAYSTACK_PK,
 
-    'baseURl' => 'https://test.gradely.ng',
+    'baseURl' => 'https://gradely.ng',
     'userImage' => "IF(image IS NULL or image = '', null,IF(image LIKE '%http%',image, CONCAT('https://gradely.ng/images/users/',image))) as image",
     'questionImage' => "IF(image IS NULL or image = '', null,IF(image LIKE '%http%',image, CONCAT('https://gradely.ng/images/questions/',image))) as image",
     'subjectImage' => "IF(image IS NULL or image = '', 'https://gradly.s3.eu-west-2.amazonaws.com/placeholders/subject.png',IF(image LIKE '%http%',image, CONCAT('https://gradely.ng/images/subjects/',image))) as image",
