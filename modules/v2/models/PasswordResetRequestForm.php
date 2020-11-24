ae05e3b54823d16d5cbef63135d6900098d8f57c
Femi Ibiwoye <femibiwoye@gmail.com>
2020-11-24 14:58:15 +0100

    Update forgot password to only not accept email

--- modules/v2/models/PasswordResetRequestForm.php
+++ modules/v2/models/PasswordResetRequestForm.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace app\modules\v2\models;
 
 use app\modules\v2\components\InputNotification;
@@ -9,49 +10,52 @@ use yii\base\Model;
 /**
  * Password reset request form
  */
-class PasswordResetRequestForm extends Model {
-	public $email;
+class PasswordResetRequestForm extends Model
+{
+    public $email;
 
-	/**
-	 * @inheritdoc
-	 */
-	public function rules() {
-		return [
-			['email', 'filter', 'filter' => 'trim'],
-			['email', 'required'],
-			['email', 'email', 'message'=> 'Provide a valid email address.'],
-			['email', 'string', 'min' => 8, 'max' => 64],
-			['email', 'match', 'pattern' => "/^[@a-zA-Z0-9._-]+$/", 'message' => "Email can only contain letters, numbers or any of these special characters [@._-]"],
-			['email', 'exist', 'targetClass' => User::className()],
-		];
-	}
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            ['email', 'filter', 'filter' => 'trim'],
+            ['email', 'required'],
+            //['email', 'email', 'message' => 'Provide a valid email address.'],
+            ['email', 'string', 'min' => 8, 'max' => 64],
+            ['email', 'match', 'pattern' => "/^[@a-zA-Z0-9.\/_-]+$/", 'message' => "Email can only contain letters, numbers or any of these special characters [/@._-]"],
+            //['email', 'exist', 'targetClass' => User::className()],
+        ];
+    }
 
-	/**
-	 * Sends an email with a link, for resetting the password.
-	 *
-	 * @return boolean whether the email was send
-	 */
-	public function sendEmail() {
-		/* @var $user User */
-		$user = User::find()
-				->where(['email' => $this->email])
-				->one();
+    /**
+     * Sends an email with a link, for resetting the password.
+     *
+     * @return boolean whether the email was send
+     */
+    public function sendEmail()
+    {
+        /* @var $user User */
+        $user = User::find()
+            ->where(['OR', ['code' => $this->email], ['email' => $this->email]])
+            ->one();
 
-		if (!$user) {
-			return false;
-		}
+        if (!$user) {
+            return false;
+        }
 
-		if (!User::isPasswordResetTokenValid($user->password_reset_token)) {
-			$user->generatePasswordResetToken();
-		}
-		
-		if (!$user->save(false)) {
-			return false;
-		}
+        if (!User::isPasswordResetTokenValid($user->password_reset_token)) {
+            $user->generatePasswordResetToken();
+        }
+
+        if (!$user->save(false)) {
+            return false;
+        }
 
         $notification = new InputNotification();
         $notification->NewNotification('request_password_reset', [['user_id', $user->id]]);
 
-		return true;
-	}
+        return true;
+    }
 }
