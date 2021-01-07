cd8963dfdba03e94c9e5a5cf5437d3efef742db3
Femi Ibiwoye <femibiwoye@gmail.com>
2021-01-07 23:40:18 +0100

    Update: Setting up CICD

--- vagrant/scripts/composer_install.sh
+++ vagrant/scripts/composer_install.sh
@@ -3,3 +3,5 @@
 sudo chmod -R 775 /var/www/test/tapi.gradely.ng
 cd /var/www/test/tapi.gradely.ng
 composer update
+cp /var/www/test/index.php /var/www/test/tapi.gradely.ng/web
+cp /var/www/test/var.php /var/www/test/tapi.gradely.ng/config
