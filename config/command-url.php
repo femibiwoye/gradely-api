86536da388b1932adc26a694cdd10a7ee78ed6a4
Femi Ibiwoye <femibiwoye@gmail.com>
2021-01-29 20:46:08 +0100

    CLass generator

--- config/command-url.php
+++ config/command-url.php
@@ -4,4 +4,6 @@ return [
 
 	//Commands
 	'GET v2/commands/video-thumbnail-extractor' => 'v2/commands/video-thumbnail-extractor',
+	'GET v2/commands/populate-school-classes' => 'v2/commands/populate-school-classes',
+
 ];
