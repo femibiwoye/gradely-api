2a623cb67a50251e0b231f55f2622e63e96c5a73
Femi Ibiwoye <femibiwoye@gmail.com>
2021-07-20 22:14:10 +0100

    Update question upload test

--- modules/v2/teacher/controllers/QuestionController.php
+++ modules/v2/teacher/controllers/QuestionController.php
@@ -215,7 +215,9 @@ class QuestionController extends ActiveController
             if (!isset($question['type']) || !in_array($question['type'], SharedConstant::QUESTION_FORMAT)) {
                 return (new ApiResponse)->error($model->getErrors(), ApiResponse::UNABLE_TO_PERFORM_ACTION, 'Type is required or invalid');
             }
-            return $question['duration'];
+            if (!isset($question['duration'])) {
+                return $question['duration'];
+            }
             $type = $question['type'];
             $question = $question['question'];
 //                $duration = $question['duration'];
