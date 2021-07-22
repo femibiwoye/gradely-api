d6e42a1608862e227665a531885002b18f5708e9
femiparadise <femibiwoye@gmail.com>
2021-07-22 20:28:39 +0100

    Add summer to payment plan

--- modules/v2/controllers/PaymentController.php
+++ modules/v2/controllers/PaymentController.php
@@ -90,7 +90,7 @@ class PaymentController extends ActiveController
     public function actionPaymentPlans($type)
     {
         $form = new \yii\base\DynamicModel(compact('type'));
-        $form->addRule(['type'], 'in', ['range' => ['catchup', 'tutor','bundle']]);
+        $form->addRule(['type'], 'in', ['range' => ['catchup', 'tutor','bundle','summer']]);
         if (!$form->validate()) {
             return (new ApiResponse)->error($form->getErrors(), ApiResponse::VALIDATION_ERROR, 'Validation failed');
         }
