aafd622cbad10533444e8cd0f54960adb3599c88
Femi Ibiwoye <femibiwoye@gmail.com>
2022-01-27 11:07:45 +0100

    Public class - Fixes

--- views/layouts/empty.php
+++ views/layouts/empty.php
@@ -3,14 +3,11 @@
 /* @var $this \yii\web\View */
 /* @var $content string */
 
-use app\widgets\Alert;
-use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
-use yii\widgets\Breadcrumbs;
-use app\assets\AppAsset;
 
-AppAsset::register($this);
+use yii\helpers\Html;
+
+
+//AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
