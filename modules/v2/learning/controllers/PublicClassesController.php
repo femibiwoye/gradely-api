ef4573953ec43a043f7807b9c6e30b8ab9534cf9
Femi Ibiwoye <femibiwoye@gmail.com>
2022-02-11 19:58:48 +0100

    Fixes

--- modules/v2/learning/controllers/PublicClassesController.php
+++ modules/v2/learning/controllers/PublicClassesController.php
@@ -38,7 +38,7 @@ class PublicClassesController extends Controller
         return $behaviors;
     }
 
-    public function actionGetClassLink($class, $is_tutor = null,$name, $image = null, $user_id = null)
+    public function actionGetClassLink($class, $is_tutor = null, $name, $image = null, $user_id = null)
     {
 
         $tutor_session = CampaignLiveClass::findOne(['class_name' => $class]);
@@ -69,23 +69,27 @@ class PublicClassesController extends Controller
         if ($bbbModel->MeetingStatus()['running'] == 'false') {
             $create = $bbbModel->CreateMeeting();
         }
-        if ($create) {
+
+//        if ($create) {
             if ($isTutor) {
                 $bbbModel->fullName = !empty($name) ? $name : $tutor_session->tutor_name;
+//                $bbbModel->moderatorPW = 'moderatorPW';
                 $bbbModel->avatarURL = !empty($image) ? $image : $tutor_session->tutor_image;
                 $bbbModel->userID = !empty($user_id) ? $user_id : $tutor_session->tutor_email;
             } else {
-                $bbbModel->fullName =$name;
+                $bbbModel->fullName = $name;
                 if (!empty($image))
                     $bbbModel->avatarURL = $image;
                 if (!empty($user_id))
                     $bbbModel->userID = $user_id;
+//                $bbbModel->attendeePW = 'attendeePW';
             }
 
 //            $destinationLink = $bbbModel->JoinMeeting($isTutor);
-        } else {
-            $destinationLink = null;
-        }
+//        } else {
+//            $destinationLink = null;
+//        }
+
         \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
         return $bbbModel->JoinMeeting($isTutor);
 
