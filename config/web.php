07228ee39263f90dd5503b8b55e29ef1f2b86f83
Femi Ibiwoye <femibiwoye@gmail.com>
2022-02-12 22:08:27 +0100

    Fixes of IP

--- config/web.php
+++ config/web.php
@@ -114,6 +114,15 @@ $config = [
     'params' => $params,
 ];
 
+if (YII_DEBUG) {
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class'      => 'yii\debug\Module',
+        'allowedIPs' => ['*'],
+    ];
+
+}
+
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
